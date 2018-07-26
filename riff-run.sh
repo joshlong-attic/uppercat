e2a49e2714de278d09fbcbd86f4e3fd32ff2eca3
joshlong <josh@joshlong.com>
2018-07-25 23:15:51 -0700

    uppercat

--- riff-run.sh
+++ riff-run.sh
@@ -1,2 +1,3 @@
 FN_NAME=${1:-uppercat}
-riff service invoke $FN_NAME  -- --header "Content-type: text/plain" -d@cat.jpg
+
+riff service invoke $FN_NAME  -- --header "Content-type: text/plain" --header "Accept: text/plain" -d"{}" -v
--- src/main/java/com/example/uppercat/UppercatApplication.java
+++ src/main/java/com/example/uppercat/UppercatApplication.java
@@ -10,24 +10,29 @@ import org.springframework.core.io.Resource;
 import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 @SpringBootApplication
 public class UppercatApplication {
 
-		private final Log log = LogFactory.getLog(getClass());
-		private final Resource resource;
 		private final byte[] bytes;
+		private final Log log = LogFactory.getLog(getClass());
+		private final Map<String, Object> result = new ConcurrentHashMap<>();
 
 		public UppercatApplication(@Value("classpath:lion.jpg") Resource resource) throws IOException {
-				this.resource = resource;
 				this.log.info("resource : " + resource.exists() + " and " + resource.getURI().toString());
 				this.bytes = StreamUtils.copyToByteArray(resource.getInputStream());
+
+				this.result.put("cat", new String(Base64.getEncoder().encode(bytes)));
+				this.result.put("key", "value");
 		}
 
 		@Bean
-		Function<byte[], byte[]> uppercat() throws IOException {
-				return in -> bytes;
+		Function<String, Map<String, Object>> uppercat() throws IOException {
+				return in -> result;
 		}
 
 		public static void main(String[] args) {
