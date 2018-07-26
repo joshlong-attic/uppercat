8176e9e788b665686c6bccb7a5f44885950af354
joshlong <josh@joshlong.com>
2018-07-26 00:01:04 -0700

    run

--- riff-run.sh
+++ riff-run.sh
@@ -1,8 +1,3 @@
-FN_NAME=${1:-uppercat}
-OUT=uppercat.json
-#riff service invoke $FN_NAME  -- --header "Content-type: text/plain"  -d"{}" -v -o $OUT
-riff service invoke $FN_NAME  -- --header "Content-type: text/plain"  -d@cat.jpg  -v -o $OUT
-rm -rf uppercat.jpg
-cat $OUT | jq -r .cat  | base64 -d  > uppercat.jpg
-
-# --header "Accept: text/plain"
+riff service invoke uppercat -- --header "Content-type: text/plain"  -d@cat.jpg  -v -o uppercat.json
+cat uppercat.json | jq -r .cat  | base64 -d  > uppercat.jpg
+#open uppercat.jpg
