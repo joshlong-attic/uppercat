265c2e87e73b304a8f9c8f253854bd35bb6b4eb7
joshlong <josh@joshlong.com>
2018-07-26 10:45:31 -0700

    up

--- riff-run.sh
+++ riff-run.sh
@@ -1,6 +1,3 @@
-#!/bin/bash 
-gimp cat.jpg &
-read -n 1
+#!/bin/bash
 riff service invoke uppercat -- --header "Content-type: text/plain"  -d@cat.jpg  -v -o uppercat.json
 cat uppercat.json | jq -r .cat  | base64 -d  > uppercat.jpg
-gimp uppercat.jpg
