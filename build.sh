37af703338c10d90f7610c0dbff310f92c5a98b5
joshlong <josh@joshlong.com>
2018-07-25 23:21:36 -0700

    up

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
 mvn -DskipTests=true clean package
 cp target/uppercat-0.0.1-SNAPSHOT.jar uppercat-0.0.1-SNAPSHOT.jar
 git commit -am up
 git push
+
+FN_NAME=${1:-uppercat}
+riff service delete ${FN_NAME}
+riff function create java $FN_NAME --git-repo https://github.com/joshlong/uppercat.git --artifact "uppercat-0.0.1-SNAPSHOT.jar" --handler "uppercat" --image gcr.io/$PROJECT_ID/$FN_NAME
+
