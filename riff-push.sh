b97754a3309fe003cfb2c270bfad8663e4f08a28
joshlong <josh@joshlong.com>
2018-07-25 23:17:00 -0700

    riff

--- riff-push.sh
+++ riff-push.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
 export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
 FN_NAME=${1:-uppercat}
+
+riff service delete ${FN_NAME}
+
 riff function create java $FN_NAME --git-repo https://github.com/joshlong/uppercat.git --artifact "uppercat-0.0.1-SNAPSHOT.jar" --handler "uppercat" --image gcr.io/$PROJECT_ID/$FN_NAME
