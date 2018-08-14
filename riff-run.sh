df97f1416789945c3598f31bd4d736d4c4cd946c
Josh Long <josh@joshlong.com>
2018-08-14 16:01:27 -0700

    up

--- riff-run.sh
+++ riff-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 riff service invoke uppercat -- --header "Content-type: text/plain"  -d@cat.jpg  -v -o uppercat.json
-cat uppercat.json | jq -r .cat  | base64 -d  > uppercat.jpg
+cat uppercat.json | jq -r .cat  | base64 > uppercat.jpg
